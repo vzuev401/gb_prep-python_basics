e1c553abdacb33a0c88137d9949515e819e2447a
vladimir zuev <vzuev401@gmail.com>
2020-01-21 02:42:33 +0700

    Added a solution for lesson_01.task_03

---
+++ lesson_01/task_03.py
@@ -0,0 +1,12 @@
+# Naming rule: n_xD, where n - str repr of number, D - count of repeats
+# Example:
+# - n_x1 means n
+# - n_x3 means nnn
+
+n_x1 = input('Input a number: ')
+
+n_x2 = n_x1 * 2
+n_x3 = n_x1 * 3
+
+print(int(n_x1) + int(n_x2) + int(n_x3))
+
