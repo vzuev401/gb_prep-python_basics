bf35be63c1c677e4a15573aa7e5b11f9b59a8c82
vladimir zuev <vzuev401@gmail.com>
2020-01-21 04:17:54 +0700

    Fixed lesson_01.task_06

--- lesson_01/task_06.py
+++ lesson_01/task_06.py
@@ -1,13 +1,12 @@
-init_distance = float(input(
+current_distance = float(input(
     'The athlete begins an everyday jogging with the distance (km): '
 ))
 desired_distance = float(input('Desired distance is .. km at least: '))
 
 day = 1
-if (current_distance := init_distance) < desired_distance:
-    while current_distance < desired_distance:
-        current_distance *= 1.1
-        day += 1
+while current_distance < desired_distance:
+    current_distance *= 1.1
+    day += 1
 
 
 print(day)
