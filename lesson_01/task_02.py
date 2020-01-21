9f56adc94a9ea905d5e664ce19f1c67c8703fdf3
vladimir zuev <vzuev401@gmail.com>
2020-01-21 18:20:28 +0700

    Replaced crutches to divmod() function

--- lesson_01/task_02.py
+++ lesson_01/task_02.py
@@ -1,31 +1,7 @@
-def get_pair_quotient_remainder(dividend, *, divisor):
-    """The pair: an integer part of division, and its remainder.
-
-    Function divides `dividend` by `divisor` and
-    returns a pair (quotient, remainder), according to
-
-    DIVIDEND = QUOTIENT * DIVISOR + REMAINDER
-
-    Parameters
-    ----------
-    dividend : int
-        Dividend
-    divisor : int
-        Divisor
-
-    Returns
-    -------
-    (int, int)
-        Pair (quotient, remainder)
-
-    """
-    return dividend // divisor, dividend % divisor
-
-
 seconds = int(input('Input a time in seconds: '))
 
-minutes, seconds = get_pair_quotient_remainder(seconds, divisor=60)
-hours, minutes = get_pair_quotient_remainder(minutes, divisor=60)
+minutes, seconds = divmod(seconds, 60)
+hours, minutes = divmod(minutes, 60)
 
 print(f'{hours:02}:{minutes:02}:{seconds:02}')
 
--- lesson_01/task_04.py
+++ lesson_01/task_04.py
@@ -1,8 +1,8 @@
 number = int(input('Input an integer: '))
 
-_num, max_digit = number // 10, number % 10
+_num, max_digit = divmod(number, 10)
 while (max_digit < 9) and (_num > 0):
-    _num, last_digit = _num // 10, _num % 10
+    _num, last_digit = divmod(_num, 10)
 
     if last_digit > max_digit:
         max_digit = last_digit
