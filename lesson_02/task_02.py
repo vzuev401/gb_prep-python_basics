e85e6ecdf3f23aa0ae318b9cd58891cb044e0577
vladimir zuev <vzuev401@gmail.com>
2020-01-24 17:52:02 +0700

    Simplified the code of lesson_02.task_02

--- lesson_02/task_02.py
+++ lesson_02/task_02.py
@@ -7,9 +7,8 @@ user_input = input(
 user_list = user_input.split(';')
 processed_user_list = user_list.copy()
 
-# `len(l) - 1` guarantees that `l[i + 1]` will exists
-for i in range(0, len(l := processed_user_list) - 1, 2):
-    l[i], l[i + 1] = l[i + 1], l[i]
+for i in range(1, len(l := processed_user_list), 2):
+    l[i - 1], l[i] = l[i], l[i - 1]
 
 print()
 print(f'Your list:      {user_list}')
